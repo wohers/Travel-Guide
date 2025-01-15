de5f0153dfc74a73116da3b6012814bc9b5a717e
danila <tulenevdanila08@gmail.com>
2025-01-15 10:52:52 +0300

    test

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -2,10 +2,17 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
 
-/ @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.Config} */
 export default [
-  { files: ['/*.{js,mjs,cjs,jsx}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'], // Исправлено: добавлены **/ для рекурсивного поиска
+    languageOptions: {
+      globals: {
+        ...globals.browser, // Глобальные переменные браузера
+        ...globals.node, // Глобальные переменные Node.js
+      },
+    },
+  },
+  pluginJs.configs.recommended, // Базовые правила ESLint
+  pluginReact.configs.flat.recommended, // Рекомендации для React
 ];
