071373443651db0ff57638f89c9d09905363c7cf
danila <tulenevdanila08@gmail.com>
2025-01-20 12:05:44 +0300

    сделан якорная кнопка

--- app/src/components/Header/Header.jsx
+++ app/src/components/Header/Header.jsx
@@ -1,13 +1,28 @@
+import React from 'react';
 import '../Header/header.scss';
 import HeaderWrap from './Wrap/HeaderWrap';
+import Button from '../../components/Button/Button';
 
 export const Header = () => {
+
+  const scroll = () => {
+    const header = document.getElementById('header');
+    if (header) {
+      header.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-    <header id="header">
-      <div className="container">
-        <HeaderWrap />
-      </div>
-    </header>
+    <>
+      <header id="header">
+        <div className="container">
+          <HeaderWrap />
+        </div>
+      </header>
+      <Button className="yakor__btn" onClick={scroll}>
+        ↑
+      </Button>
+    </>
   );
 };
 
