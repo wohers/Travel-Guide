18d16997562ac454dfa12b96891b6dfa6dbb5135
danila <tulenevdanila08@gmail.com>
2025-01-20 23:52:39 +0300

    bugfix

--- app/src/pages/Attractions/Attractions.jsx
+++ app/src/pages/Attractions/Attractions.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import ArticleManager from './ArticleManager';
 import Search from './components/Search';
 import Filter from './components/Filter';
@@ -8,8 +10,45 @@ import Pagination from './components/Pagination';
 import './Attractions.scss';
 import Header from '../../components/Header/Header';
 import { Footer } from '../../components/Footer/Footer';
+import Button from '../../components/Button/Button';
 
 const Attractions = () => {
+  const [isHeaderVisible, setIsHeaderVisible] = useState(true);
+
+  const scroll = () => {
+    const header = document.getElementById('header');
+    const footer = document.getElementById('footer');
+
+    if (header && footer) {
+      const headerRect = header.getBoundingClientRect();
+
+      if (headerRect.top >= 0 && headerRect.bottom <= window.innerHeight) {
+        footer.scrollIntoView({ behavior: 'smooth' });
+        setIsHeaderVisible(false);
+      } else {
+        header.scrollIntoView({ behavior: 'smooth' });
+        setIsHeaderVisible(true);
+      }
+    } else {
+      console.error('Header или Footer не найдены в DOM');
+    }
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.getElementById('header');
+      if (header) {
+        const headerRect = header.getBoundingClientRect();
+        setIsHeaderVisible(
+          headerRect.top >= 0 && headerRect.bottom <= window.innerHeight
+        );
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   const baseUrl =
     'https://67322e8b2a1b1a4ae10f29a6.mockapi.io/guide/v1/articles';
   const {
@@ -41,7 +80,7 @@ const Attractions = () => {
 
   return (
     <>
-      <Header />
+      <Header id="header" />
       <section className="main">
         <div className="main__container">
           <div className="main__title-container">
@@ -58,7 +97,10 @@ const Attractions = () => {
           <Pagination currentPage={currentPage} onPageChange={setCurrentPage} />
         </div>
       </section>
-      <Footer />
+      <Footer id="footer" />
+      <Button className="yakor__btn" onClick={scroll}>
+        {isHeaderVisible ? '↓' : '↑'}
+      </Button>
     </>
   );
 };
